2c3a86f4d8ed5f63d8051fdf11fc5aec297a60c1
umairahmadmumtaz <umairahmadmumtaz@gmail.com>
2025-07-12 16:25:32 +0500

    hide projects

--- src/portfolio.js
+++ src/portfolio.js
@@ -283,7 +283,7 @@ const bigProjects = {
       ]
     }
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Achievement Section
